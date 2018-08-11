af7862990a5023c958b467a5644af4c77da67993
Piraveena Paralogarajah <piraveena18@gmail.com>
2018-08-11 21:43:58 +0530

    Update and rename logout.sh to bclogout.sh

--- bclogout.sh
+++ bclogout.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
+
 mkdir bclogout
 cd bclogout
 
 git clone https://github.com/wso2-extensions/identity-inbound-auth-oauth.git
 cd identity-inbound-auth-oauth
-git checkout origin/feature-oidc-back-channel-logout
+git checkout feature-oidc-backchannel-logout
 mvn clean install -Dmaven.test.skip=true
 
 cd -
 
 git clone https://github.com/wso2/product-is.git
 cd product-is
-git checkout origin/feature-oidc-back-channel-logout
-mvn clean install -Dmaven.test.skip=true
-
-echo Register http://playground2/logout as back-channel-logout url via management console. 
+git checkout feature-oidc-backchannel-logout
+mvn clean install -Dmaven.test.skip=true 
