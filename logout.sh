c5af876e17a99989fb0fcf3f0ab5a464c68039fa
Piraveena Paralogarajah <piraveena18@gmail.com>
2018-08-11 21:31:56 +0530

    Update logout.sh

--- logout.sh
+++ logout.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+mkdir bclogout
+cd bclogout
 
 git clone https://github.com/wso2-extensions/identity-inbound-auth-oauth.git
 cd identity-inbound-auth-oauth
 git checkout origin/feature-oidc-back-channel-logout
 mvn clean install -Dmaven.test.skip=true
+
+cd -
 
 git clone https://github.com/wso2/product-is.git
 cd product-is
