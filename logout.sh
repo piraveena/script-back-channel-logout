70b4eebad2b54fca8297946fb0bd75531439cfc2
Piraveena Paralogarajah <piraveena18@gmail.com>
2018-08-11 21:21:38 +0530

    Update and rename my-script.sh to logout.sh

---
+++ logout.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git clone https://github.com/wso2-extensions/identity-inbound-auth-oauth.git
+cd identity-inbound-auth-oauth
+git checkout origin/feature-oidc-back-channel-logout
+mvn clean install -Dmaven.test.skip=true
+
+git clone https://github.com/wso2/product-is.git
+cd product-is
+git checkout origin/feature-oidc-back-channel-logout
+mvn clean install -Dmaven.test.skip=true
+
+echo Register http://playground2/logout as back-channel-logout url via management console. 
